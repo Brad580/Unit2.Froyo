0ac8eea626756644a778ed7b74fc40bee389b89e
Brad Travers <bradtravers8740@gmail.com>
2023-11-13 18:18:54 -0500

    froyo.js

--- froyo.js
+++ froyo.js
@@ -1,8 +1,9 @@
 function trackFroyoFlavors(userInput) {
-    const froyoFlavors = userInput.split(",");
-    const chosenFlavors = {};
+    const froyoFlavors =  userInput.split(",")
+        ;
+    const chosenFlavors = {} ;
 
-    for (const flavor of froyoFlavors) {
+    for  (const flavor of froyoFlavors) {
         const trimmedFlavor = flavor.trim();
         chosenFlavors[trimmedFlavor] = (chosenFlavors[trimmedFlavor] || 0) + 1;
     }
@@ -10,14 +11,15 @@ function trackFroyoFlavors(userInput) {
     return chosenFlavors;
 }
 
-const flavorsInput = prompt("Enter the flavors of Froyo you'd like to order, just put a comma in between each flavor:");
+const flavorsInput = prompt("Enter the flavors of Froyo you'd like to order, just put a comma in between each flavor:")
+    ;
 
 const chosenFlavors = trackFroyoFlavors(flavorsInput);
 
-console.log("Input Froyo Flavors:", flavorsInput);
+console.log ("Input Froyo Flavors:", flavorsInput);
 
 const flavorsDiv = document.getElementById("flavors");
-flavorsDiv.innerHTML = "<h2>Chosen Froyo Flavors:</h2><ul>";
+flavorsDiv.innerHTML = "<h2> Chosen Froyo Flavors :</h2><ul>";
 for (const flavor in chosenFlavors) {
     flavorsDiv.innerHTML += `<li>${flavor}: ${chosenFlavors[flavor]}</li>`;
 }
